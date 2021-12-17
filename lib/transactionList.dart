fb5218955ce50c12a65cff9dcfe6504ab309e968
duypham <xduy.pham@gmail.com>
2021-12-17 23:17:11 +0700

    widget create list

--- lib/transactionList.dart
+++ lib/transactionList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'transaction.dart';
 
 class TransactionList extends StatelessWidget {
@@ -6,36 +7,38 @@ class TransactionList extends StatelessWidget {
 
   TransactionList({required this.transactions});
 
-  List<Widget> _buildWidgetList() {
-    int index = 0;
-    return transactions.map((e) {
-      index++;
-      return Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        color: (index - 1) % 2 == 0 ? Colors.lightBlue : Colors.greenAccent,
-        child: ListTile(
-          leading: const Icon(
-            Icons.alarm,
-            color: Colors.white,
-          ),
-          title: Text(
-            e.content,
-            style: TextStyle(color: Colors.white),
-          ),
-          subtitle: Text(
-            'Price: ${e.amount}',
-            style: TextStyle(color: Colors.white),
-          ),
-          onTap: () {
-            print('tab me');
-          },
-        ),
-      );
-    }).toList();
+  ListView _buildWidgetList() {
+    return ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (context, index) {
+          return Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            color: (index - 1) % 2 == 0 ? Colors.lightBlue : Colors.green,
+            child: ListTile(
+              leading: const Icon(
+                Icons.alarm,
+                color: Colors.white,
+              ),
+              title: Text(
+                transactions[index].content,
+                style: TextStyle(color: Colors.white),
+              ),
+              subtitle: Text(
+                'Price: ${transactions[index].amount}',
+                style: TextStyle(color: Colors.white),
+              ),
+              onTap: () {
+                print('tab me');
+              },
+            ),
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: _buildWidgetList());
+    // return Column(children: _buildWidgetList());
+    return Container(height: 500, child: _buildWidgetList());
   }
 }
